ffcad1854a304652dfb2a249e0f77631ac0f5bc1
jeocarlolubao <jeocarlolubao@gmail.com>
2023-11-05 18:18:24 +0800

    updated meta tags

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -6,22 +6,22 @@
 	<title>FAQ</title>
 
 	<!-- Primary Meta Tags -->
-	<meta name="title" content="HCDC True Site" />
+	<meta name="title" content="HCDC True Site: Are HCDC websites down?" />
 	<meta name="description" content="HCDC True Site: Are HCDC websites down?" />
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://hcdctruesite.vercel.app/" />
-	<meta property="og:title" content="HCDC True Site" />
+	<meta property="og:title" content="HCDC True Site: Are HCDC websites down?" />
 	<meta property="og:description" content="HCDC True Site: Are HCDC websites down?" />
-	<meta property="og:image" content="img/linkpreview.png" />
+	<meta property="og:image" content="https://i.imgur.com/Fl4qygc.png" />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="https://hcdctruesite.vercel.app/" />
-	<meta property="twitter:title" content="HCDC True Site" />
+	<meta property="twitter:title" content="HCDC True Site: Are HCDC websites down?" />
 	<meta property="twitter:description" content="HCDC True Site: Are HCDC websites down?" />
-	<meta property="twitter:image" content="img/linkpreview.png" />
+	<meta property="twitter:image" content="https://i.imgur.com/Fl4qygc.png" />
 
 	<!-- Meta Tags Generated with https://metatags.io -->
 </svelte:head>
